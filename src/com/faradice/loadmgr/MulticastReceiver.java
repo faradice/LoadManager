df239c3759360aebd6706fdab7dc28235d4ef81b
ragnarvald <ragnar@decode.is>
2018-06-17 11:05:19 +0000

    LoadManager using multicast

---
+++ src/com/faradice/loadmgr/MulticastReceiver.java
@@ -0,0 +1,30 @@
+package com.faradice.loadmgr;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class MulticastReceiver extends Thread {
+	protected byte[] buf = new byte[256];
+	InetAddress group = null;
+	MulticastSocket socket = null;
+
+	@Override
+	public void run() {
+		try (MulticastSocket socket = new MulticastSocket(4446)) {
+			group = InetAddress.getByName("230.0.0.0");
+			socket.joinGroup(group);
+			while (true) {
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+				String received = new String(packet.getData(), 0, packet.getLength());
+				if ("end".equals(received)) {
+					break;
+				}
+			}
+			socket.leaveGroup(group);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
